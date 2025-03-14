a6acbcac34b53f8b53f1d53b2cd3d97e02067373
Rob Cleaton <rob@warface.co.uk>
2025-03-14 12:28:58 +0000

    repo name added to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,14 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  // Use a conditional basePath only in production
+  basePath: process.env.NODE_ENV === 'production' ? '/tailwind-v0' : '',
+  // Disable image optimization for static export
   images: {
     unoptimized: true,
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-}
+  // Ensure trailing slashes are handled correctly
+  trailingSlash: true,
+};
 
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
-
-export default nextConfig
+export default nextConfig;
